ce7d075a6fc351a3e723bf79ab3c761a98282b2e
Miazak1 <vinicius.santos@sptech.school>
2024-11-11 05:49:37 -0300

    atividades feitas

---
+++ Login/script.js
@@ -0,0 +1,11 @@
+const register = document.querySelector('.sign-up-link')
+const login = document.querySelector('.log-in-link')
+const registerForm = document.querySelector('.form-section-register')
+const loginForm = document.querySelector('.form-section-login')
+register.addEventListener('click', () => {
+    if(registerForm == 'display: none'){
+        registerForm == 'display: flex'
+    } else {
+        
+    }
+});
